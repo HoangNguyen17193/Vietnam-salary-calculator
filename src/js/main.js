c6cf2c67bf186dec3d80ca334a63979d256d6b23
HoangNguyen17193 <hoangnh17193@gmail.com>
2019-08-07 14:14:26 +0700

    validate input

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 import Calculator from './calculator.js';
 
-Waves.attach('.button');
-Waves.init();
 
-$('.TC__form').on('submit', (e) => {
-  e.preventDefault();
-  const grossSalary = $('.TC__form__gross-salary').val();
-  const dependents = $('.TC__form__register-dependents').val();
-  Calculator.calculate(grossSalary, dependents);
+
+
+$(document).ready(() => {
+  Waves.attach('.button');
+  Waves.init();
+  new AutoNumeric('#gross-salary', {
+    unformatOnSubmit: true,
+    minimumValue: 0
+  });
+
+  $('.TC__form').on('submit', (e) => {
+    e.preventDefault();
+    const grossSalary = $('.TC__form__gross-salary').val();
+    const dependents = $('.TC__form__register-dependents').val();
+    Calculator.calculate(grossSalary, dependents);
+  });
 });
