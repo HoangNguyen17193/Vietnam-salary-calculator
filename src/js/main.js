8d666c6a7cdf2395df7e2981829d983770e71ba8
HoangNguyen17193 <hoangnh17193@gmail.com>
2019-08-20 10:22:30 +0700

    update UI, add insurance salary input

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,19 @@ import Report from './service/report.js';
 $(document).ready(() => {
   Waves.attach('.TC__form__button');
   Waves.init();
-  new AutoNumeric('#gross-salary', {
+  new AutoNumeric('.TC__form__gross-salary', {
     unformatOnSubmit: true,
     minimumValue: 0
+  });
+
+  new AutoNumeric('.TC__form__insurance__pay-for__input', {
+    unformatOnSubmit: true,
+    minimumValue: 0
+  });
+
+  $('input[name=insurance-pay-for-radio-input]').change(function() {
+    console.log(this);
+    $('.TC__form__insurance__pay-for__input').attr('disabled', this.value === "full");
   });
 
   $('.TC__form').on('submit', (e) => {
@@ -15,7 +25,6 @@ $(document).ready(() => {
     const dependents = $('.TC__form__register-dependents').val();
     const region = $('input[name=region-radio-input]:checked').val();
     const salaryDetail = Calculator.calculate(grossSalary, dependents, region);
-    $('html').width("700px");
     $('.TC__result-container').empty();
     $('.TC__result-container').append(Report.generate(salaryDetail));
   });
@@ -25,6 +34,5 @@ $(document).ready(() => {
     $('.TC__form__register-dependents').val(0);
     $('.TC__result-container').empty();
     $("#TC_region1").prop("checked", true);
-    $('html').width("510px");
   })
 });
